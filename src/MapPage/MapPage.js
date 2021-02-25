138ee73b077198a19600606c5b306b5c7ae4b9e9
flitterio <francesca@litterio.net>
2021-02-25 17:42:01 -0500

    made map with locations on it from tutorial

--- src/MapPage/MapPage.js
+++ src/MapPage/MapPage.js
@@ -2,8 +2,19 @@ import React, {Component} from 'react';
 import {GoogleMap, useLoadScript,  InfoWindow, Marker} from '@react-google-maps/api';
 import { formatRelative } from 'date-fns';
 
+import usePlacesAutocomplete, {
+    getGeocode,
+    getLatLng,
+} from 'use-places-autocomplete';
+import {
+    Combobox,
+    ComboboxInput,
+    ComboboxPopover,
+    ComboboxList,
+    ComboboxOption,
+} from '@reach/combobox';
+
 import '@reach/combobox/styles.css';
-import { FaTextHeight } from 'react-icons/fa';
 import mapStyles from './mapStyles';
 
 
@@ -15,40 +26,152 @@ const mapContainerStyle ={
 const center = {
     lat: 40.7831,
     lng: -73.9712,
-}
+};
 const options = {
     styles: mapStyles,
     disableDefaultUI: true,
     zoomControl: true
-}
+}; 
+
 export default function App() {
     const {isLoaded, loadError} = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_MAP_API_KEY,
        libraries
     });
     const [markers, setMarkers] = React.useState([]);
+    const [selected, setSelected] = React.useState(null);
+
+    const onMapClick = React.useCallback((event) => {
+        setMarkers(current => [...current, {
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng(),
+            time: new Date()
+            },
+        ]);
+    }, []);
+
+    const mapRef = React.useRef();
+    const onMapLoad = React.useCallback((map) => {
+        mapRef.current = map;
+    }, []);
+
+    const panTo = React.useCallback(({lat, lng}) => {
+        mapRef.current.panTo({lat, lng});
+        mapRef.current.setZoom(14);
+    }, []);
 
     if(loadError) return "Error loading maps";
     if(!isLoaded) return 'Loading Maps';
     return (
         <div>
+            <Search panTo={panTo}/>
+            <Locate panTo={panTo}/>
+
             <h1 className='pnyc'>PNYC </h1>
             <GoogleMap 
                 mapContainerStyle={mapContainerStyle}
                 zoom={12}
                 center={center}
                 options={options}
-                onClick={(event) => {
-                    setMarkers(current => [...current, {
-                        lat: event.latLng.lat(),
-                        lng: event.latLng.lng(),
-                        time: new Date()
-                        },
-                    ]);
-                }}
+                onClick={onMapClick}
+                onLoad={onMapLoad}
             >
-                    {markers.map(marker => <Marker key={marker.lat + marker.lng} position={{lat: marker.lat, lng: marker.lng}}/> )}
+                    {markers.map((marker) => (
+                    <Marker 
+                        key={marker.lat + marker.lng} 
+                        position={{lat: marker.lat, lng: marker.lng}}
+                        icon={{
+                            url:'./pile-of-poo_1f4a9.png',
+                            scaledSize: new window.google.maps.Size(40, 40),
+                            origin: new window.google.maps.Point(0,0),
+                            anchor: new window.google.maps.Point(15, 15)
+                        }}
+                        onClick={() => {
+                            setSelected(marker);
+                        }}
+                    /> 
+                ))}
+
+                {selected ? (
+                    <InfoWindow 
+                        position={{lat: selected.lat, lng: selected.lng}} 
+                        onCloseClick={() => {
+                            setSelected(null);
+                            }}
+                    >
+                        <div>
+                            <h2>Public Bathroom</h2>
+                            <p>Added {formatRelative(selected.time, new Date())}</p>
+                        </div>
+                </InfoWindow>) : null}
             </GoogleMap>
         </div>
+    )
+}
+
+function Locate({panTo}) {
+    return (
+        <button className='locate' onClick={() => {
+            navigator.geolocation.getCurrentPosition((position) => {
+                panTo({
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                });
+            },
+             () => null, options);
+        }}>
+            <img src='./current-location.png' alt ='find current location' />
+        </button>
+    )
+}
+
+function Search({panTo}){
+    const {
+        ready, 
+        value, 
+        suggestions: {status, data}, 
+        setValue, 
+        clearSuggestions,
+    } = usePlacesAutocomplete({
+        requestOptions: {
+            location: {lat: () => 40.7831, lng: () => -73.9712, },
+            radius: 200 * 1000,
+        },
+    });
+
+    return (
+    <div className='search'>
+        <Combobox 
+            onSelect={async (address) => {
+                setValue(address, false);
+                clearSuggestions();
+
+                try{
+                    const results = await getGeocode({address});
+                    const { lat, lng} = await getLatLng(results[0]);
+                    panTo({ lat, lng });
+
+                }catch(error) {
+                    console.log('error!');
+                }
+            }}
+        >
+            <ComboboxInput 
+                value={value} 
+                onChange={(e) => {
+                    setValue(e.target.value);
+                }} 
+                disabled={!ready}
+                placeholder="Enter an address"
+            />
+            <ComboboxPopover>
+                <ComboboxList>
+                {status === 'OK' && data.map(({id, description}) =>
+                     <ComboboxOption key = {id} value={description} />
+                     )}
+                </ComboboxList>
+            </ComboboxPopover>
+        </Combobox> 
+    </div> 
     )
 }
